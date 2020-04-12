7a38b6e8e346d4cf5826ddffaf0f3bc19a2dd850
Vijay Kumar <50508427+VijayKumarKTG@users.noreply.github.com>
2020-04-12 17:09:59 +0630

    First commit for the markdown previewer project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import marked from 'marked';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange() {
+    let textarea = document.querySelector('#editor');
+    let code = textarea.value;
+    console.log(code);
+    let value = marked(code);
+    let preview = document.querySelector('#preview');
+    preview.innerHTML = value;
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <i className="fab fa-markdown"></i> Simple: A Markdown Previewer
+        </header>
+        <div className="editor">
+          <div className="head">
+            <i className="fas fa-edit"></i> Editor
+          </div>
+          <textarea
+            defaultValue="# Please enter your markdown codes here"
+            id="editor"
+            onChange={this.handleChange}
+          ></textarea>
+        </div>
+        <div className="previewer">
+          <div className="head">
+            <i className="fas fa-search"></i> Previewer
+          </div>
+          <div id="preview">Your code output will be shown here</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
